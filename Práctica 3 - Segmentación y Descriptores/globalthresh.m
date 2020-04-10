b4f5b4e8477d26f68df1d2922ab5c48002864e1a
Sawn39 <33158086+Sawn39@users.noreply.github.com>
2020-04-10 21:16:22 +0200

    p3 terminada

--- Práctica 3 - Segmentación y Descriptores/globalthresh.m
+++ Práctica 3 - Segmentación y Descriptores/globalthresh.m
@@ -5,22 +5,20 @@ function [imagen, tf] = globalthresh(img, t0, et)
 
 %Averiguamos el tamanyo de la matriz
 [m,n] = size(img);
-diferencia = 100;
 tf = t0;
 
 %Calculamos el nuevo umbral
-while diferencia>=et
+while tf>=et
     m1 = 0;
     cont1 = 0;
     m2 = 0;
     cont2 = 0;
-    tanterior = tf;
     for x = 1:m
         for y = 1:n
-            if img(x,y)>tf
+            if img(x,y)>tf %Claros
                 m1 = m1 + img(x,y);
                 cont1 = cont1 + 1;
-            else
+            else           %Oscuros
                 m2 = m2 + img(x,y);
                 cont2 = cont2 + 1;
             end
@@ -33,7 +31,6 @@ while diferencia>=et
     
     %Nuevo umbral
     tf = 0.5*(m1+m2);
-    diferencia = abs(tf-tanterior);
 end
 
 %Creamos la imagen
